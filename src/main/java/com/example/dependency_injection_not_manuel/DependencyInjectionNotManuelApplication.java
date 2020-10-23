ac94a3495363e632a33a6f188398c0ac81712c92
zahitkaya <zahit_kaya58@hotmail.com>
2020-10-23 16:05:02 +0300

    Spring Boot dependency injection types

--- src/main/java/com/example/dependency_injection_not_manuel/DependencyInjectionNotManuelApplication.java
+++ src/main/java/com/example/dependency_injection_not_manuel/DependencyInjectionNotManuelApplication.java
@@ -1,19 +1,18 @@
 package com.example.dependency_injection_not_manuel;
 
-import com.example.dependency_injection_not_manuel.constructor_type.Bmw;
-import com.example.dependency_injection_not_manuel.constructor_type.Car;
-import com.example.dependency_injection_not_manuel.constructor_type.IEngine;
+import com.example.dependency_injection_not_manuel.property_type.Computer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionNotManuelApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DependencyInjectionNotManuelApplication.class, args);
-        IEngine engine=new Bmw();
-        Car c=new Car(engine);
-        engine.turnOn();
+        ApplicationContext ctx=SpringApplication.run(DependencyInjectionNotManuelApplication.class, args);
+        Computer computer= (Computer) ctx.getBean(Computer.class);
+        computer.start();
+
 
     }
 
